af60a8598719f8972a353ca998169d1452153a80
André <andrevictor50@gmail.com>
2021-02-16 17:42:19 -0300

    Creating first components with story and tests + add test helper

---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,8 @@
+import styled, { css } from 'styled-components';
+import { LogoProps } from '.';
+
+export const Wrapper = styled.div<LogoProps>`
+  ${({ theme, color }) => css`
+    color: ${theme.colors[color]};
+  `}
+`;
