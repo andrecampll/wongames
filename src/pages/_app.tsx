ac2ea7689af5454e570d942fdf302677863f3327
André <andrevictor50@gmail.com>
2021-02-16 16:09:41 -0300

    Using theme with themeprovider and adding it to storybook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
+import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../styles/GlobalStyles';
+import theme from '../styles/theme';
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Component {...pageProps} />
       <Head>
         <link rel="manifest" href="/manifest.json" />
@@ -11,6 +14,6 @@ export default function App({ Component, pageProps }) {
       </Head>
 
       <GlobalStyles />
-    </>
+    </ThemeProvider>
   );
 }
--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,62 +1,63 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, css } from 'styled-components';
 
 export default createGlobalStyle`
-  @font-face {
-    font-family: 'Poppins';
-    font-style: normal;
-    font-weight: 300;
-    font-display: swap;
-    src: local('Poppins Light'), local('Poppins-Light'),
+  ${({ theme }) => css`
+    @font-face {
+      font-family: 'Poppins';
+      font-style: normal;
+      font-weight: 300;
+      font-display: swap;
+      src: local('Poppins Light'), local('Poppins-Light'),
         url('/fonts/poppins-v12-latin-300.woff2') format('woff2');
-  }
-  @font-face {
-    font-family: 'Poppins';
-    font-style: normal;
-    font-weight: 400;
-    font-display: swap;
-    src: local('Poppins Regular'), local('Poppins-Regular'),
-        url('/fonts/poppins-v12-latin-regular.woff2') format('woff2');
-  }
-  @font-face {
-    font-family: 'Poppins';
-    font-style: normal;
-    font-weight: 600;
-    font-display: swap;
-    src: local('Poppins SemiBold'), local('Poppins-SemiBold'),
-        url('/fonts/poppins-v12-latin-600.woff2') format('woff2');
-  }
-
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    outline: 0;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-
-  body {
-    background: #282C34;
-
-    h1 {
-      color: #fff;
     }
-  }
+    @font-face {
+      font-family: 'Poppins';
+      font-style: normal;
+      font-weight: 400;
+      font-display: swap;
+      src: local('Poppins Regular'), local('Poppins-Regular'),
+        url('/fonts/poppins-v12-latin-regular.woff2') format('woff2');
+    }
+    @font-face {
+      font-family: 'Poppins';
+      font-style: normal;
+      font-weight: 600;
+      font-display: swap;
+      src: local('Poppins SemiBold'), local('Poppins-SemiBold'),
+        url('/fonts/poppins-v12-latin-600.woff2') format('woff2');
+    }
 
-  body, input, button, textarea {
-    font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    font-size: 16px;
-  }
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      outline: 0;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+    }
 
-  button {
-    border: 0;
-  }
+    body {
+      background: ${theme.colors.mainBg};
+      font-size: ${theme.font.sizes.medium};
 
-  html, body, #__next {
-    height: 100%;
-  }
+      h1 {
+        color: #fff;
+      }
+    }
 
-  html {
-    font-size: 62.5%;
-  }
+    body,
+    input,
+    button,
+    textarea {
+      font-family: ${theme.font.family};
+    }
+
+    button {
+      border: 0;
+    }
+
+    html {
+      font-size: 62.5%;
+    }
+  `}
 `;
