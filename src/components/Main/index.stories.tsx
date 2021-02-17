84ab9edc1bf030ca4f92fd765693e92bb8161b66
André <andrevictor50@gmail.com>
2021-02-17 08:46:10 -0300

    Removing unused components for 100% test coverage

--- src/components/Main/index.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0';
-import Main from './index';
-
-export default {
-  title: 'Main',
-  component: Main,
-} as Meta;
-
-export const Basic: Story = () => <Main />;
--- src/components/Main/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Wrapper } from './styles';
-
-const Main = () => (
-  <Wrapper>
-    <h1>Hello</h1>
-  </Wrapper>
-);
-
-export default Main;
