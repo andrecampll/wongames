c500a69def53665b869badbe0b57f030d9846c6c
André <andrevictor50@gmail.com>
2021-02-20 10:44:59 -0300

    Adding tests for button variations

---
+++ src/__tests__/components/Button/index.spec.tsx
@@ -0,0 +1,35 @@
+import { screen } from '@testing-library/react';
+import Button from '../../../components/Button';
+import { renderWithTheme } from '../../../utils/tests/helpers';
+import 'jest-styled-components';
+
+describe('Button', () => {
+  it('should be able to render a medium Button by default', () => {
+    renderWithTheme(<Button>Buy now</Button>);
+
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
+      padding: '0.8rem 3.2rem',
+      height: '4rem',
+      'font-size': '1.4rem',
+    });
+  });
+
+  it('should be able to render a small Button', () => {
+    renderWithTheme(<Button size="small">Buy now</Button>);
+
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
+      height: '3rem',
+      'font-size': '1.2rem',
+    });
+  });
+
+  it('should be able to render a large Button', () => {
+    renderWithTheme(<Button size="large">Buy now</Button>);
+
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
+      height: '5rem',
+      'font-size': '1.6rem',
+      padding: '0.8rem 4.8rem',
+    });
+  });
+});
