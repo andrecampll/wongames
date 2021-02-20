5bf725c8346a953e57af4c99960b0c57c09eb8d9
André <andrevictor50@gmail.com>
2021-02-20 12:26:40 -0300

    Adding mobile version for Menu

---
+++ src/components/Menu/index.stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+import Menu from './index';
+
+export default {
+  title: 'Menu',
+  component: Menu,
+} as Meta;
+
+export const Basic: Story = () => <Menu />;
