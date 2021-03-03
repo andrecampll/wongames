e1b2bfa636590498fd3a79be0f0039d4fea21287
André <andrevictor50@gmail.com>
2021-03-03 08:06:14 -0300

    Adding float image for highlight

--- src/components/Highlight/index.stories.tsx
+++ src/components/Highlight/index.stories.tsx
@@ -1,16 +1,30 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
-import Highlight, { HighlightProps } from './index';
+import Highlight, { HighlightProps } from '.';
 
 export default {
   title: 'Highlight',
   component: Highlight,
   args: {
-    title: 'Read Dead it’s back',
+    title: 'Read Dead is back!',
     subtitle: 'Come see John’s new adventures',
     backgroundImage: '/img/red-dead-img.jpg',
     buttonLabel: 'Buy now',
-    buttonLink: '/rdr2',
+    buttonLink: '/games/rdr2',
   },
 } as Meta;
 
-export const Basic: Story<HighlightProps> = args => <Highlight {...args} />;
+export const Basic: Story<HighlightProps> = args => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+);
+
+export const WithFloatImage: Story<HighlightProps> = args => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+);
+
+WithFloatImage.args = {
+  floatImage: '/img/red-dead-float.png',
+};
