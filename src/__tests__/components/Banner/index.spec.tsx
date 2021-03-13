ce5e0c83d3ba052e795faa19b02c768b55209d96
André Victor <53975579+andrecampll@users.noreply.github.com>
2021-03-12 22:35:26 -0300

    Update index.spec.tsx

--- src/__tests__/components/Banner/index.spec.tsx
+++ src/__tests__/components/Banner/index.spec.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react';
+import { screen } from '@testing-library/react'
 import { renderWithTheme } from '../../../utils/tests/helpers';
 
 import Banner from '../../../components/Banner';
