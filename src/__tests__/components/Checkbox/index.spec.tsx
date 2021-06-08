9a99aaee3bbd7947e1524838d79ee15a59957987
André <andrevictor50@gmail.com>
2021-06-07 23:28:53 -0300

    Adding dynamic label color + tests (CheckBox)

--- src/__tests__/components/Checkbox/index.spec.tsx
+++ src/__tests__/components/Checkbox/index.spec.tsx
@@ -3,11 +3,27 @@ import { renderWithTheme } from '../../../utils/tests/helpers';
 import Checkbox from '../../../components/Checkbox';
 
 describe('Checkbox', () => {
-  it('should be able to render', () => {
+  it('should be able to render with a label', () => {
     renderWithTheme(<Checkbox label="checkbox label" labelFor="check" />);
 
     expect(screen.getByRole('checkbox')).toBeInTheDocument();
     expect(screen.getByLabelText(/checkbox label/i)).toBeInTheDocument();
     expect(screen.getByText(/checkbox label/i)).toHaveAttribute('for', 'check');
+  });
+
+  it('should not be able to render with a label', () => {
+    renderWithTheme(<Checkbox />);
+
+    expect(screen.queryByLabelText('Checkbox')).not.toBeInTheDocument();
+  });
+
+  it('should be able to render with black label', () => {
+    renderWithTheme(
+      <Checkbox label="checkbox label" labelFor="check" labelColor="black" />,
+    );
+
+    expect(screen.getByText(/checkbox label/i)).toHaveStyle({
+      color: '#030517',
+    });
   });
 });
--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,15 +1,24 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import { Wrapper } from './styles';
+import { Wrapper, Label } from './styles';
 
 export type CheckboxProps = {
   label?: string;
   labelFor?: string;
+  labelColor?: 'white' | 'black';
 };
 
-const Checkbox = ({ label, labelFor = '' }: CheckboxProps) => (
+const Checkbox = ({
+  label,
+  labelFor = '',
+  labelColor = 'white',
+}: CheckboxProps) => (
   <Wrapper>
     <input id={labelFor} type="checkbox" />
-    {!!label && <label htmlFor={labelFor}>{label}</label>}
+    {!!label && (
+      <Label htmlFor={labelFor} labelColor={labelColor}>
+        {label}
+      </Label>
+    )}
   </Wrapper>
 );
 
