a5e3e334890823f72c24dbcfcd9eb5ec91029205
André <andrevictor50@gmail.com>
2021-06-22 19:21:48 -0300

    Adding font variation for Heading + tests

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -22,7 +22,7 @@ const Auth = ({ title, children }: AuthProps) => (
         <Logo id="banner" />
 
         <div>
-          <Heading>All your favorite games in one place</Heading>
+          <Heading size="huge">All your favorite games in one place</Heading>
           <Subtitle>
             <strong>WON</strong> is the best and most complete gaming platform
           </Subtitle>
