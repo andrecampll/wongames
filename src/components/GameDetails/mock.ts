f4c201e08c94d826937190752cee868d5798b670
andrecampll <andrevictor50@gmail.com>
2021-07-04 15:36:20 -0300

    Adjust Storybook for GameDetails

--- src/components/GameDetails/mock.ts
+++ src/components/GameDetails/mock.ts
@@ -1,7 +1,7 @@
 export default {
   developer: 'Different Tales',
   releaseDate: '2020-11-21T23:00:00',
-  platform: ['windows', 'mac', 'linux'],
+  platforms: ['windows', 'mac', 'linux'],
   publisher: 'Walkabout',
   rating: 'BR0',
   genres: ['Role-playing'],
