26342e4b3997ffedfea4bf8f3e8cc02e7a04efdf
andrecampll <andrevictor50@gmail.com>
2021-07-05 22:05:34 -0300

    Adding Open method for Gallery component + tests

--- src/__tests__/components/Gallery/index.spec.tsx
+++ src/__tests__/components/Gallery/index.spec.tsx
@@ -1,13 +1,36 @@
 /* eslint-disable import/no-unresolved */
 import 'match-media-mock';
+import { fireEvent, screen } from '@testing-library/react';
 import { renderWithTheme } from '../../../utils/tests/helpers';
+
 import items from '../../../components/Gallery/mock';
 import Gallery from '../../../components/Gallery';
 
-describe('Gallery', () => {
-  it('should be able to render', () => {
-    renderWithTheme(<Gallery items={items} />);
+describe('<Gallery />', () => {
+  it('should be able to render thumbnails as buttons', () => {
+    renderWithTheme(<Gallery items={items.slice(0, 2)} />);
 
-    expect(1 + 1).toBe(2);
+    expect(
+      screen.getByRole('button', { name: /thumb - gallery image 1/i }),
+    ).toHaveAttribute('src', items[0].src);
+
+    expect(
+      screen.getByRole('button', { name: /thumb - gallery image 2/i }),
+    ).toHaveAttribute('src', items[1].src);
+  });
+
+  it('should be able to render open modal when the user click at the modal', () => {
+    renderWithTheme(<Gallery items={items.slice(0, 2)} />);
+
+    const modal = screen.getByLabelText('modal');
+
+    expect(modal.getAttribute('aria-hidden')).toBe('true');
+    expect(modal).toHaveStyle({ opacity: 0, pointerEvents: 'none' });
+
+    fireEvent.click(
+      screen.getByRole('button', { name: /thumb - gallery image 1/i }),
+    );
+    expect(modal.getAttribute('aria-hidden')).toBe('false');
+    expect(modal).toHaveStyle({ opacity: 1 });
   });
 });
--- src/components/Gallery/styles.ts
+++ src/components/Gallery/styles.ts
@@ -37,3 +37,24 @@ export const Wrapper = styled.div`
     `}
   `}
 `;
+
+type ModalProps = {
+  isOpen: boolean;
+};
+
+const modalModifiers = {
+  open: () => css`
+    opacity: 1;
+  `,
+  close: () => css`
+    opacity: 0;
+    pointer-events: none;
+  `,
+};
+
+export const Modal = styled.div<ModalProps>`
+  ${({ isOpen }) => css`
+    ${isOpen && modalModifiers.open()}
+    ${!isOpen && modalModifiers.close()}
+  `}
+`;
