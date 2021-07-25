27b0363192b9c8fc2e6b3afe6cc78e03bf37f753
andrecampll <andrevictor50@gmail.com>
2021-07-25 13:12:33 -0300

    Adding CartList Dropdown

---
+++ src/components/CartDropdown/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { Wrapper as CartListWrapper } from '../CartList/styles';
+
+export const Wrapper = styled.div`
+  ${CartListWrapper} {
+    width: 56rem;
+  }
+`;
