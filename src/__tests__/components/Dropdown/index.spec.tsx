3182aa8dbc057ac3b537822e6c2c7397386521ce
andrecampll <andrevictor50@gmail.com>
2021-07-25 12:10:56 -0300

    Adding Dropdown component]

---
+++ src/__tests__/components/Dropdown/index.spec.tsx
@@ -0,0 +1,32 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { renderWithTheme } from '../../../utils/tests/helpers';
+import Dropdown from '../../../components/Dropdown';
+
+describe('Dropdown', () => {
+  beforeEach(() => {
+    const title = <h1 aria-label="toggle dropdown">Click here</h1>;
+
+    renderWithTheme(
+      <Dropdown title={title}>
+        <span>content</span>
+      </Dropdown>,
+    );
+  });
+
+  it('should be able to render title', () => {
+    expect(screen.getByLabelText('toggle dropdown'));
+  });
+
+  it('should be able to handle open close dropdown', () => {
+    const content = screen.getByText(/content/).parentElement;
+
+    expect(content).toHaveStyle({ opacity: 0 });
+    expect(content.getAttribute('aria-hidden')).toBe('true');
+
+    userEvent.click(screen.getByLabelText(/toggle dropdown/i));
+
+    expect(content).toHaveStyle({ opacity: 1 });
+    expect(content.getAttribute('aria-hidden')).toBe('false');
+  });
+});
