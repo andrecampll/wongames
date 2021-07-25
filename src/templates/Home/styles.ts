d80df9997034e6868a14de790c6d529cde603798
andrecampll <andrevictor50@gmail.com>
2021-07-25 16:02:27 -0300

    Adding Dropdown and link to Menu

--- src/templates/Home/styles.ts
+++ src/templates/Home/styles.ts
@@ -6,7 +6,7 @@ import * as HighlightStyles from '../../components/Highlight/styles';
 
 export const SectionBanner = styled.section`
   ${({ theme }) => css`
-    margin: 0 ${theme.spacings.large} calc(-${theme.grid.gutter} / 2);
+    margin: 0 calc(-${theme.grid.gutter} / 2) ${theme.spacings.large};
     ${media.greaterThan('medium')`
       margin-bottom: ${theme.spacings.large};
       position: relative;
