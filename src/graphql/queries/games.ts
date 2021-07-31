7899b1fdf3fbd14488e9bb199ddc26276d689d78
andrecampll <andrevictor50@gmail.com>
2021-07-31 17:03:25 -0300

    Adding new QueryGameBySlug GraphQL query + SSG

--- src/graphql/queries/games.ts
+++ src/graphql/queries/games.ts
@@ -18,3 +18,40 @@ export const QUERY_GAMES = gql`
     }
   }
 `;
+
+export const QUERY_GAMES_BY_SLUG = gql`
+  query QueryGameBySlug($slug: String!) {
+    games(where: { slug: $slug }) {
+      name
+      short_description
+      description
+      price
+      rating
+      release_date
+      gallery {
+        src: url
+        label: alternativeText
+      }
+
+      cover {
+        src: url
+      }
+
+      developers {
+        name
+      }
+
+      publisher {
+        name
+      }
+
+      categories {
+        name
+      }
+
+      platforms {
+        name
+      }
+    }
+  }
+`;
