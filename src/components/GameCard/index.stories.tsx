c0146c395d49d4684fcdfd0e0f9dc9820a4f5c9e
andrecampll <andrevictor50@gmail.com>
2021-07-31 16:37:27 -0300

    Adding slug to game card component

--- src/components/GameCard/index.stories.tsx
+++ src/components/GameCard/index.stories.tsx
@@ -5,6 +5,7 @@ export default {
   title: 'GameCard',
   component: GameCard,
   args: {
+    slug: 'population-zero',
     title: 'Population Zero',
     developer: 'Rockstar Games',
     image: 'https://source.unsplash.com/user/willianjusten/300x140',
