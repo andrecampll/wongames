d6be7d97aba661860c9f63538870eaa7945bea41
andrecampll <andrevictor50@gmail.com>
2021-08-03 23:47:34 -0300

    Adding newGames GraphQL query and generated types

--- src/graphql/queries/home.ts
+++ src/graphql/queries/home.ts
@@ -1,12 +1,22 @@
 import { gql } from '@apollo/client';
 import { BannerFragment } from '../fragments/banner';
+import { GameFragment } from '../fragments/game';
 
 export const QUERY_HOME = gql`
   query QueryHome {
     banners {
       ...BannerFragment
     }
+
+    newGames: games(
+      where: { release_date_lte: "2020-08-01" }
+      sort: "release_date:desc"
+      limit: 8
+    ) {
+      ...GameFragment
+    }
   }
 
   ${BannerFragment}
+  ${GameFragment}
 `;
