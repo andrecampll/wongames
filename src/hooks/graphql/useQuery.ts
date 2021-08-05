eb052efc6722fd7b48053333fa2d206e7d8abbe9
andrecampll <andrevictor50@gmail.com>
2021-08-05 11:00:23 -0300

    Refactoring useQuery custom hook and use RecommendedGames hook

--- src/hooks/graphql/useQuery.ts
+++ src/hooks/graphql/useQuery.ts
@@ -1,20 +1,20 @@
-import { QueryHookOptions, useQuery as useApolloQuery } from '@apollo/client';
-import { DocumentNode } from 'graphql';
 import { useCallback, useState } from 'react';
+import { DocumentNode } from 'graphql';
+import { QueryHookOptions, useQuery as useApolloQuery } from '@apollo/client';
 
 export const useQuery = <Result = unknown, Variables = Record<string, unknown>>(
   query: DocumentNode,
-  options: QueryHookOptions<Result, Variables>,
+  options: QueryHookOptions<Result, Variables> = {},
 ) => {
   const [refetching, setRefetching] = useState(false);
 
-  const { refetch: _refetch, ...result } = useApolloQuery<Result, Variables>(
-    query,
-    {
-      nextFetchPolicy: 'cache-first',
-      ...options,
-    },
-  );
+  const { error, refetch: _refetch, ...result } = useApolloQuery<
+    Result,
+    Variables
+  >(query, {
+    nextFetchPolicy: 'cache-first',
+    ...options,
+  });
 
   const refetch = useCallback(
     (variables?: Variables) => {
@@ -27,6 +27,7 @@ export const useQuery = <Result = unknown, Variables = Record<string, unknown>>(
   return {
     refetch,
     refetching,
+    error,
     ...result,
   };
 };
