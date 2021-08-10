ea7dd4b1a92efd5ce7c807181a005ea7141bf936
andrecampll <andrevictor50@gmail.com>
2021-08-09 23:17:54 -0300

    Adding useQuery custom hook to games page

--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -1,4 +1,12 @@
 import { KeyboardArrowDown as ArrowDown } from '@styled-icons/material-outlined/KeyboardArrowDown';
+// import { useQuery } from '@apollo/client';
+import {
+  QueryGames,
+  QueryGamesVariables,
+} from '../../graphql/generated/QueryGames';
+import { useQuery } from '../../hooks/graphql/useQuery';
+import { QUERY_GAMES } from '../../graphql/queries/games';
+
 import Base from '../Base';
 import ExploreSidebar, { ItemProps } from '../../components/ExploreSidebar';
 import GameCard, { GameCardProps } from '../../components/GameCard';
@@ -11,7 +19,16 @@ export type GamesTemplateProps = {
   filterItems: ItemProps[];
 };
 
-const Games = ({ games = [], filterItems }: GamesTemplateProps) => {
+const Games = ({ filterItems }: GamesTemplateProps) => {
+  const { data, loading } = useQuery<QueryGames, QueryGamesVariables>(
+    QUERY_GAMES,
+    {
+      variables: {
+        limit: 15,
+      },
+    },
+  );
+
   const handleFilter = () => ({});
 
   const handleShowMore = () => ({});
@@ -21,18 +38,33 @@ const Games = ({ games = [], filterItems }: GamesTemplateProps) => {
       <Main>
         <ExploreSidebar items={filterItems} onFilter={handleFilter} />
 
-        <section>
-          <Grid>
-            {games.map(item => (
-              <GameCard key={item.title} {...item} />
-            ))}
-          </Grid>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <section>
+            <Grid>
+              {data?.games.map(({ name, slug, developers, cover, price }) => (
+                <GameCard
+                  key={slug}
+                  title={name}
+                  slug={slug}
+                  developer={developers[0].name}
+                  image={
+                    cover?.url
+                      ? `http://localhost:1337${cover?.url}`
+                      : 'https://i.stack.imgur.com/y9DpT.jpg'
+                  }
+                  price={price}
+                />
+              ))}
+            </Grid>
 
-          <ShowMore role="button" onClick={handleShowMore}>
-            <p>Show More</p>
-            <ArrowDown size={35} />
-          </ShowMore>
-        </section>
+            <ShowMore role="button" onClick={handleShowMore}>
+              <p>Show More</p>
+              <ArrowDown size={35} />
+            </ShowMore>
+          </section>
+        )}
       </Main>
     </Base>
   );
