f9dee2fc4a5f02ce1c68bd4ad640cb04bb257915
andrecampll <andrevictor50@gmail.com>
2021-08-26 23:22:18 -0300

    Adding ids to GameFragment

--- src/graphql/queries/games.ts
+++ src/graphql/queries/games.ts
@@ -22,6 +22,7 @@ export const QUERY_GAMES = gql`
 export const QUERY_GAMES_BY_SLUG = gql`
   query QueryGameBySlug($slug: String!) {
     games(where: { slug: $slug }) {
+      id
       name
       short_description
       description
