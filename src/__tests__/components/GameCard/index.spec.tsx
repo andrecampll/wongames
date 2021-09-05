97aab9f0c72cfb3cadd65cb02782fff4540d817f
andrecampll <andrevictor50@gmail.com>
2021-09-05 14:48:50 -0300

    Integrate CartButton

--- src/__tests__/components/GameCard/index.spec.tsx
+++ src/__tests__/components/GameCard/index.spec.tsx
@@ -7,6 +7,7 @@ import theme from '../../../styles/theme';
 import 'jest-styled-components';
 
 const props = {
+  id: '1',
   slug: 'population-zero',
   title: 'Population Zero',
   developer: 'Rockstar Games',
