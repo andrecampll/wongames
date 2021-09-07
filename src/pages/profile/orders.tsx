7a3d64604e98e417b1e98a8113c0a64817727761
andrecampll <andrevictor50@gmail.com>
2021-09-07 15:38:32 -0300

    Adding protected routes, calbackUrl redirect flow + tests

--- src/pages/profile/orders.tsx
+++ src/pages/profile/orders.tsx
@@ -1,7 +1,9 @@
+import { GetServerSidePropsContext } from 'next';
 import Profile from '../../templates/Profile';
 import OrdersList, { OrdersListProps } from '../../components/OrdersList';
 
 import ordersMock from '../../components/OrdersList/mock';
+import protectedRoutes from '../../utils/protectedUtils';
 
 export default function Orders({ items }: OrdersListProps) {
   return (
@@ -11,10 +13,13 @@ export default function Orders({ items }: OrdersListProps) {
   );
 }
 
-export function getServerSideProps() {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await protectedRoutes(context);
+
   return {
     props: {
       items: ordersMock,
+      session,
     },
   };
 }
