26b6563c7f4b1a05b3796a67a93ce56d7b9d341d
andrecampll <andrevictor50@gmail.com>
2021-09-07 14:24:03 -0300

    Configure NextAuth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,60 @@
+import NextAuth, { User, Session } from 'next-auth';
+import { JWT } from 'next-auth/jwt';
+import Providers from 'next-auth/providers';
+import { NextApiRequest, NextApiResponse } from 'next-auth/internals/utils';
+
+type AuthorizeProps = {
+  email: string;
+  password: string;
+};
+
+const options = {
+  pages: {
+    signIn: '/sign-in',
+  },
+  providers: [
+    Providers.Credentials({
+      name: 'Sign-in',
+      credentials: {},
+      async authorize({ email, password }: AuthorizeProps) {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/auth/local`,
+          {
+            method: 'POST',
+            body: new URLSearchParams({ identifier: email, password }),
+          },
+        );
+
+        const data = await response.json();
+
+        if (data.user) {
+          return { ...data.user, jwt: data.jwt };
+        }
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    session: async (session: Session, user: User) => {
+      session.jwt = user.jwt;
+      session.id = user.id;
+
+      return Promise.resolve(session);
+    },
+    jwt: async (token: JWT, user: User) => {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+        token.name = user.username as string;
+        token.jwt = user.jwt;
+      }
+
+      return Promise.resolve(token);
+    },
+  },
+};
+
+const Auth = (req: NextApiRequest, res: NextApiResponse) =>
+  NextAuth(req, res, options);
+
+export default Auth;
