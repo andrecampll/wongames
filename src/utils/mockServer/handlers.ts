8af72e64706dfe38c05e8776d82d376bf051f9d5
andrecampll <andrevictor50@gmail.com>
2021-09-09 23:00:02 -0300

    Add FormForgotPassword and FormResetPassword tests + MSW handlers

--- src/utils/mockServer/handlers.ts
+++ src/utils/mockServer/handlers.ts
@@ -1,1 +1,79 @@
-export const handlers = [];
+import { rest } from 'msw';
+
+type LoginReqBody = {
+  email: string;
+};
+
+type ResetReqBody = {
+  code: string;
+  password: string;
+  passwordConfirmation: string;
+};
+
+export const handlers = [
+  rest.post<LoginReqBody>(
+    `${process.env.NEXT_PUBLIC_API_URL}/auth/forgot-password`,
+    (req, res, ctx) => {
+      const { email } = req.body;
+
+      if (email === 'false@email.com') {
+        return res(
+          ctx.status(400),
+          ctx.json({
+            error: 'Bad Request',
+            message: [
+              {
+                messages: [
+                  {
+                    message: 'This email does not exist',
+                  },
+                ],
+              },
+            ],
+          }),
+        );
+      }
+
+      return res(
+        ctx.status(200),
+        ctx.json({
+          ok: true,
+        }),
+      );
+    },
+  ),
+
+  rest.post<ResetReqBody>(
+    `${process.env.NEXT_PUBLIC_API_URL}/auth/reset-password`,
+    (req, res, ctx) => {
+      const { code } = req.body;
+
+      if (code === 'wrong_code') {
+        return res(
+          ctx.status(400),
+          ctx.json({
+            error: 'Bad Request',
+            message: [
+              {
+                messages: [
+                  {
+                    message: 'Incorrect code provided.',
+                  },
+                ],
+              },
+            ],
+          }),
+        );
+      }
+
+      return res(
+        ctx.status(200),
+        ctx.json({
+          user: {
+            email: 'valid@email.com',
+          },
+        }),
+      );
+    },
+  ),
+];
