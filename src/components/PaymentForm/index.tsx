4a9f44b46c655f60c44776034b0eaa11de61730a
andrecampll <andrevictor50@gmail.com>
2021-09-19 19:19:50 -0300

    Adding styles to CardElement, add disable button state and starting create paymentIntent

--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -1,12 +1,35 @@
+import {
+  useState,
+  // useEffect
+} from 'react';
 import { CardElement } from '@stripe/react-stripe-js';
-import { ShoppingCart } from '@styled-icons/material-outlined';
+import { StripeCardElementChangeEvent } from '@stripe/stripe-js';
+import { ErrorOutline, ShoppingCart } from '@styled-icons/material-outlined';
+// import { useCart } from '../../hooks/cart/useCart';
 
 import Button from '../Button';
 import Heading from '../Heading';
 
-import { Wrapper, Body, Footer } from './styles';
+import { Wrapper, Body, Footer, Error } from './styles';
 
 const PaymentForm = () => {
+  // const { items } = useCart();
+  const [error, setError] = useState<string | null>(null);
+  const [disabled, setDisabled] = useState(true);
+  // const [clientSecret, setClientSecret] = useState('');
+  // const [freeGames, setFreeGames] = useState(false);
+
+  const handleChange = async (event: StripeCardElementChangeEvent) => {
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : '');
+  };
+
+  // useEffect(() => {
+  //   if (items.length) {
+
+  //   }
+  // }, [items]);
+
   return (
     <Wrapper>
       <Body>
@@ -14,14 +37,35 @@ const PaymentForm = () => {
           Payment
         </Heading>
 
-        <CardElement options={{ hidePostalCode: true }} />
+        <CardElement
+          options={{
+            hidePostalCode: true,
+            style: {
+              base: {
+                fontSize: '16px',
+              },
+            },
+          }}
+          onChange={handleChange}
+        />
+
+        {error && (
+          <Error>
+            <ErrorOutline size={20} />
+            {error}
+          </Error>
+        )}
       </Body>
 
       <Footer>
         <Button as="a" fullWidth minimal>
           Continue shopping
         </Button>
-        <Button fullWidth icon={<ShoppingCart />}>
+        <Button
+          fullWidth
+          icon={<ShoppingCart />}
+          disabled={disabled || !!error}
+        >
           Buy now
         </Button>
       </Footer>
